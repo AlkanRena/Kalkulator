30e992ccb5d4dd0ef444fe2078fc6f9f8126926f
AlkanRena <dawid.kubic@gmail.com>
2017-03-21 21:33:42 +0100

    Math is working :)

--- kalkulator/src/app/app.component.ts
+++ kalkulator/src/app/app.component.ts
@@ -20,29 +20,35 @@ import { Component } from '@angular/core';
                 <div class="col-xs-6" *ngIf="showInput">
                     <div class="col-xs-12" *ngIf="count.formulaVal === 'circuit'">Obwód
                     <div class="col-xs-9" *ngIf="count.figureVal === 'Koło'">
-                    Promień: <input>
+                    Promień: <input [(ngModel)]="variable" value="{{variable}}">
+                    <button (click)="resultCircuitCircle()">Policz obwód</button>
                     </div>
                     <div class="col-xs-9" *ngIf="count.figureVal === 'Kwadrat'">
-                    Bok: <input>
+                    Bok: <input [(ngModel)]="variable" value="{{variable}}">
+                    <button (click)="resultCircuitSquare()">Policz obwód</button>
                     </div>
                     <div class="col-xs-9" *ngIf="count.figureVal === 'Prostokąt'">
-                    Bok a: <input>
-                    Bok b: <input>
+                    Bok a: <input [(ngModel)]="variable" value="{{variable}}">
+                    Bok b: <input [(ngModel)]="variableB" value="{{variableB}}">
+                    <button (click)="resultCircuitRectangle()">Policz obwód</button>
                     </div>
                     </div>
                     <div class="col-xs-12" *ngIf="count.formulaVal === 'area'">Pole
                     <div class="col-xs-9" *ngIf="count.figureVal === 'Koło'">
-                    Promień: <input>
+                    Promień: <input [(ngModel)]="variable" value="{{variable}}">
+                    <button (click)="resultAreaCircle()">Policz pole</button>
                     </div>
                     <div class="col-xs-9" *ngIf="count.figureVal === 'Kwadrat'">
-                    Bok: <input>
+                    Bok: <input [(ngModel)]="variable" value="{{variable}}">
+                    <button (click)="resultAreaSquare()">Policz pole</button>
                     </div>
                     <div class="col-xs-9" *ngIf="count.figureVal === 'Prostokąt'">
-                    Bok a: <input>
-                    Bok b: <input>
+                    Bok a: <input [(ngModel)]="variable" value="{{variable}}">
+                    Bok b: <input [(ngModel)]="variableB" value="{{variableB}}">
+                    <button (click)="resultAreaRectangle()">Policz pole</button>
                     </div>
                     </div>
-                    Wynik: <input>
+                    Wynik: <input [(ngModel)]="result" value="{{result}}">
                 </div>
                 <button (click)="toggleInput()">{{showInput ? "Wróc do wyboru" : "Przejdź do obliczeń"}}</button>`,
   styleUrls: ['./app.component.css']
@@ -53,6 +59,9 @@ export class AppComponent {
   geometric_figure : string[];
   formulas : string[];
   showInput : boolean;
+  variable : number;
+  variableB : number;
+  result : number;
 
 
 constructor(){
@@ -64,6 +73,9 @@ constructor(){
   this.geometric_figure = ['Wybierz', 'Koło', 'Kwadrat', 'Prostokąt'];
   this.formulas = ['circuit', 'area'];
   this.showInput = false;
+  this.variable = 0;
+  this.variableB = 0;
+  this.result = 0;
 }
 
   changeFigure(value) {
@@ -73,8 +85,35 @@ constructor(){
   toggleInput(){
     if (this.showInput == true){
       this.showInput = false;
+      this.variable = 0;
+      this.variableB = 0;
+      this.result = 0;
     } else {
       this.showInput = true;
     }
+  }
+
+  resultCircuitCircle(){
+    this.result = 2 * Math.PI * this.variable;
+  }
+
+  resultCircuitSquare(){
+    this.result = this.variable * 4;
+  }
+
+  resultCircuitRectangle() {
+    this.result = this.variable * 2 + this.variableB * 2;
+  }
+
+  resultAreaCircle(){
+    this.result = Math.PI * Math.pow(this.variable, 2);
+  }
+
+  resultAreaSquare(){
+    this.result = Math.pow(this.variable, 2);
+  }
+
+  resultAreaRectangle(){
+    this.result = this.variable * this.variableB;
   }
 }
